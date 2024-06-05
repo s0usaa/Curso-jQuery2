5fd515cd6b5bb6af31f7d00ce1cc3f7243a56114
s0usaa <sousavlc86@gmail.com>
2024-06-05 08:58:06 +0200

    Hacer una llamada a un archivo externo con AJAX a traves de una funcion

--- ejemplo.php
+++ ejemplo.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
 
-$mensaje = "la solicitud fue un exito";
+$mensaje = "la solicitud fue un exito, por fin";
 
 echo $mensaje;
 
 ?>
 
-</body>
-</html>
 
 
