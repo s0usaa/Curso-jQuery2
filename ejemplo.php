bc1ff9e17167849e8f33843d23322abbf15a2dcb
s0usaa <sousavlc86@gmail.com>
2024-06-05 19:30:01 +0200

    Convertir un JSON de php en un array y poder mostrarlo en un archivo HTML

--- ejemplo.php
+++ ejemplo.php
@@ -1,13 +1,16 @@
 <?php
-// obtener los datos del formulario
-$nombre = $_POST['nombre'] ?? '';
-$email = $_POST['email'] ?? '';
-$mensaje = $_POST['mensaje'] ?? '';
 
-// Realizar la operacion con los datos
-$respuesta = "Formulario recibido! Nombre: {$nombre}, Email : {$email}, Mensaje: {$mensaje}";
+$datos = array(
 
-echo $respuesta;
+    'nombre' => 'Luis',
+    'edad' => 34,
+    'pais' => 'España'
+);
+
+header('Content-Type: application/json');
+
+echo json_encode($datos);
+
 ?>
 
 
