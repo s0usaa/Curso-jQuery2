cdc67c8d6b6ba5ca5f1464d755197fc7484e0a0b
s0usaa <sousavlc86@gmail.com>
2024-06-26 19:36:08 +0200

    Galeria de imagenes que se acciona mediante tres botones, que hacen que cambien las imagenes

--- cosas.js
+++ cosas.js
@@ -1,115 +1,70 @@
-var IsNotCal = false;
+let index = 1;
+let state = "stop";
 
 $(document).ready(function(){
-    $(".op").click(function(){
-        let num = $(this).attr("id");
+    $(".small").click(function(){
+        $("#"+index).removeClass("selected");
+        let source = $(this).attr("src");
+        $(".big").attr("src", source);
 
-        clicKey(num);
+        index = parseInt($(this).attr("id"));
+        $("#"+index).addClass("selected");
     });
 
-    $('#result').click(function(){
-        getResult();
-    });
-
-    $('#clear').click(function(){
-        clearOutput();
+    $("#backward").click(function(){
+        goBack();
     })
 
-    $('#del').click(function(){
-        deleteKey();
+    $("#forward").click(function(){
+        goNext();
     })
 
     $(document).keydown(function(e){
-        switch (e.key) {
-            case '1':
-                clicKey('1');
-                break;
-             case '2':
-                clicKey('2');
-                    break;
-             case '3':
-                clicKey('3');
-                    break;
-             case '4':
-                clicKey('4');
-                    break;
-             case '5':
-                clicKey('5');
-                    break;
-             case '6':
-                clicKey('6');
-                    break;
-             case '7':
-                clicKey('7');
-                    break;
-             case '8':
-                clicKey('8');
-                    break;
-             case '9':
-                clicKey('9');
-                    break;
-             case '0':
-                clicKey('0');
-                    break;
-             case '.':
-                clicKey('.');
-                    break;
-             case '+':
-                clicKey('+');
-                    break;
-             case '-':
-                clicKey('-');
-                    break;
-             case '*':
-                clicKey('*');
-                    break;
-             case '/':
-                clicKey('/');
-                    break;
-             case 'Backspace':
-                e.preventDefault();
-                deleteKey();
-                    break;
-             case 'Enter':
-                e.preventDefault();
-                getResult();
-                    break;
-            default:
-                break;
+        if(e.key == "ArrowLeft"){
+            goBack();
+        }else if(e.key == "ArrowRight"){
+            goNext();
+        }
+    })
+
+    $("#play").click(function(){
+        if(state == "stop"){
+            state = "run";
+            $("#play").text("#");
+            interval = setInterval(goNext, 5000);
+        }else{
+            state = "stop";
+            $("#play").text(">");
+            clearInterval(interval);
         }
     })
 });
 
-// funcion para q muestre en el div la tecla que pulsamos 
-function clicKey(num){
 
-    if(IsNotCal == true){
-        $('#output').text('');
-    }
+function goBack(){
+    $("#"+index).removeClass("selected");
 
-    let str = $('#output').text();
-    $('#output').text(str + num);
-    IsNotCal = false;
-
-}
-
-function getResult(){
-    IsNotCal = true;
-    let numResult = eval($('#output').text());
-    $('#output').text(numResult);
-}
-
-function clearOutput(){
-    $('#output').text('');
-}
-
-function deleteKey(){
-    
-    if(IsNotCal == true){
-        $('#output').text('');
+    if(index == 1){
+        index = 5;
     }else{
-        let str = $('#output').text();
-        str = str.substr(0, str.length - 1);
-        $('#output').text(str);
+        index = index -1
     }
+    
+    let source = $("#"+index).attr("src");
+    $(".big").attr("src", source);
+    $("#"+index).addClass("selected");
+}
+
+function goNext(){
+    $("#"+index).removeClass("selected");
+
+    if(index == 5){
+        index = 1;
+    }else{
+        index = index + 1
+    }
+
+    let source = $("#"+index).attr("src");
+    $(".big").attr("src", source);
+    $("#"+index).addClass("selected");
 }
