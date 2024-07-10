41367cb0f7310100a3a9aad99881dda0e1c81b99
David Soto Sousa <sousavlc86@gmail.com>
2024-07-10 18:59:00 +0200

    Galeria de imagenes para motrar distintas fotos

--- cosas.js
+++ cosas.js
@@ -1,70 +1,13 @@
-let index = 1;
-let state = "stop";
 
 $(document).ready(function(){
-    $(".small").click(function(){
-        $("#"+index).removeClass("selected");
-        let source = $(this).attr("src");
-        $(".big").attr("src", source);
+    $(".filters button").on("click", function(){
+        let filtroValor = $(this).attr("data-filter");
 
-        index = parseInt($(this).attr("id"));
-        $("#"+index).addClass("selected");
-    });
-
-    $("#backward").click(function(){
-        goBack();
-    })
-
-    $("#forward").click(function(){
-        goNext();
-    })
-
-    $(document).keydown(function(e){
-        if(e.key == "ArrowLeft"){
-            goBack();
-        }else if(e.key == "ArrowRight"){
-            goNext();
-        }
-    })
-
-    $("#play").click(function(){
-        if(state == "stop"){
-            state = "run";
-            $("#play").text("#");
-            interval = setInterval(goNext, 5000);
+        if(filtroValor === 'all'){
+            $(".image").show();
         }else{
-            state = "stop";
-            $("#play").text(">");
-            clearInterval(interval);
+            $(".image").hide();
+            $('.image[data-category="'+filtroValor +'"]').show();
         }
     })
-});
-
-
-function goBack(){
-    $("#"+index).removeClass("selected");
-
-    if(index == 1){
-        index = 5;
-    }else{
-        index = index -1
-    }
-    
-    let source = $("#"+index).attr("src");
-    $(".big").attr("src", source);
-    $("#"+index).addClass("selected");
-}
-
-function goNext(){
-    $("#"+index).removeClass("selected");
-
-    if(index == 5){
-        index = 1;
-    }else{
-        index = index + 1
-    }
-
-    let source = $("#"+index).attr("src");
-    $(".big").attr("src", source);
-    $("#"+index).addClass("selected");
-}
+})
